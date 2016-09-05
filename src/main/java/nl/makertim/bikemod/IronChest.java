7bbb6d208c6ded84fb040f13a480d4883fcdd350
MakerTim <MakerTim@outlook.com>
2016-09-05 10:47:49 +0200

    made some progress

--- src/main/java/nl/makertim/bikemod/IronChest.java
+++ src/main/java/nl/makertim/bikemod/IronChest.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2012 cpw.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- * <p>
- * Contributors:
- * cpw - initial API and implementation
- ******************************************************************************/
 package cpw.mods.ironchest;
 
 import java.util.Properties;
@@ -21,48 +11,45 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = IronChest.MOD_ID, name = "Iron Chests", dependencies = "required-after:Forge@[12.17.0.1909,)", acceptedMinecraftVersions = "[1.9.4, 1.11)")
-public class IronChest
-{
-    public static final String MOD_ID = "ironchest";
+@Mod(modid = ModInfo.ID, name = "Iron Chests", dependencies = "required-after:Forge@[12.17.0.1909,)", acceptedMinecraftVersions = "[1.9.4, 1.11)")
+public class IronChest {
+	public static final String MOD_ID = "ironchest";
 
-    @Instance(IronChest.MOD_ID)
-    public static IronChest instance;
+	@Instance(IronChest.MOD_ID)
+	public static IronChest instance;
 
-    @SidedProxy(clientSide = "cpw.mods.ironchest.client.ClientProxy", serverSide = "cpw.mods.ironchest.CommonProxy")
-    public static CommonProxy proxy;
+	@SidedProxy(clientSide = "cpw.mods.ironchest.client.ClientProxy", serverSide = "cpw.mods.ironchest.CommonProxy")
+	public static CommonProxy proxy;
 
-    public static BlockIronChest ironChestBlock;
-    public static ItemIronChest ironChestItemBlock;
+	public static BlockIronChest ironChestBlock;
+	public static ItemIronChest ironChestItemBlock;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        Properties properties = event.getVersionProperties();
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		Properties properties = event.getVersionProperties();
 
-        if (properties != null)
-        {
-            String major = properties.getProperty("IronChest.build.major.number");
-            String minor = properties.getProperty("IronChest.build.minor.number");
-            String rev = properties.getProperty("IronChest.build.revision.number");
-            String build = properties.getProperty("IronChest.build.number");
-            // String mcversion = properties.getProperty("IronChest.build.mcversion");
-            event.getModMetadata().version = String.format("%s.%s.%s build %s", major, minor, rev, build);
-        }
+		if (properties != null) {
+			String major = properties.getProperty("IronChest.build.major.number");
+			String minor = properties.getProperty("IronChest.build.minor.number");
+			String rev = properties.getProperty("IronChest.build.revision.number");
+			String build = properties.getProperty("IronChest.build.number");
+			// String mcversion =
+			// properties.getProperty("IronChest.build.mcversion");
+			event.getModMetadata().version = String.format("%s.%s.%s build %s", major, minor, rev, build);
+		}
 
-        ChestChangerType.buildItems();
-        ironChestBlock = GameRegistry.register(new BlockIronChest());
-        ironChestItemBlock = GameRegistry.register(new ItemIronChest(ironChestBlock));
+		ChestChangerType.buildItems();
+		ironChestBlock = GameRegistry.register(new BlockIronChest());
+		ironChestItemBlock = GameRegistry.register(new ItemIronChest(ironChestBlock));
 
-        for (IronChestType typ : IronChestType.VALUES)
-        {
-            GameRegistry.registerTileEntity(typ.clazz, "IronChest." + typ.name());
-        }
+		for (IronChestType typ : IronChestType.VALUES) {
+			GameRegistry.registerTileEntity(typ.clazz, "IronChest." + typ.name());
+		}
 
-        IronChestType.registerBlocksAndRecipes(ironChestBlock);
-        ChestChangerType.generateRecipes();
-        NetworkRegistry.INSTANCE.registerGuiHandler(instance, proxy);
-        proxy.registerRenderInformation();
-        MinecraftForge.EVENT_BUS.register(new OcelotsSitOnChestsHandler());
-    }
+		IronChestType.registerBlocksAndRecipes(ironChestBlock);
+		ChestChangerType.generateRecipes();
+		NetworkRegistry.INSTANCE.registerGuiHandler(instance, proxy);
+		proxy.registerRenderInformation();
+		MinecraftForge.EVENT_BUS.register(new OcelotsSitOnChestsHandler());
+	}
 }
---
+++ src/main/java/nl/makertim/bikemod/ModInfo.java
@@ -0,0 +1,12 @@
+package nl.makertim.bikemod;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ModInfo {
+
+	public static final String MOD_ID = "bikemod";
+	public static final String NAME = "Bikes";
+	public static final String VERSION = "${version}";
+
+}
